4ce4e430f98fd56ff4745bdbb62b4fc40306035b
omen <kybar089@gmail.com>
2022-10-09 22:58:43 -0500

    connected to 2nd weather API/added event listener

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
-var apiKey = "219508d53c09462ba3344f4b9b6bbf02";
-var userInput = document.getElementById('search-input').value
-var h3 = document.querySelector('h3')
+var userInput = document.getElementById('search-input')
+var search = document.getElementById('search-button')
 
-
-fetch("http://api.openweathermap.org/geo/1.0/direct?q=milwaukee&limit=5&appid=219508d53c09462ba3344f4b9b6bbf02")
+function searchWeather(userInput) {
+    var apiKey = "219508d53c09462ba3344f4b9b6bbf02";
+    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=5&appid=" + apiKey)
     .then(function(response) {
-        return response.json()
+       return response.json()
     })
     .then(function(jsonData) {
         console.log(jsonData)
         //create variables that will store Latitude and longitude jsonData results
-        var latitude = jsonData[0].lat
-        var longitude = jsonData[0].lon
+        var lat = jsonData[0].lat.value;
+        var lon = jsonData[0].lon.value;
         //append to local storage
+        localStorage.setItem("lat", lat);
+        localStorage.setItem("lon", lon);
+    fetch("api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey)
+    .then(function(responses) {
+        return responses.json()
+        })
+    .then(function(data) {
+        console.log(data)
+        })
     })
+}
+search.addEventListener("click",function() {
+    var citySearch = userInput.value;
+    searchWeather(citySearch);
+})
+    searchWeather()
     
-    
-    // localStorage.setItem("lat", lat);
-    // localStorage.setItem("lon", lon);
     // h2.innerText = jsonData[0].lat
     // h3.innerText = jsonData[0].lon
     
