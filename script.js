2fdeb811d00276c30dde497c58ae9fe2eeef7b5d
omen <kybar089@gmail.com>
2022-10-10 23:31:44 -0500

    updated searchWeather function

--- script.js
+++ script.js
@@ -1,35 +1,38 @@
 var userInput = document.getElementById('search-input')
 var search = document.getElementById('search-button')
+var currentCity = document.getElementById('current-city')
+var currentTemp = document.getElementById('current-temp')
 
 function searchWeather(userInput) {
     var apiKey = "219508d53c09462ba3344f4b9b6bbf02";
-    fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&limit=5&appid=" + apiKey)
-    .then(function(response) {
-       return response.json()
-    })
-    .then(function(jsonData) {
-        console.log(jsonData)
-        //create variables that will store Latitude and longitude jsonData results
-        var lat = jsonData[0].lat.value;
-        var lon = jsonData[0].lon.value;
-        //append to local storage
-        localStorage.setItem("lat", lat);
-        localStorage.setItem("lon", lon);
-    fetch("api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + apiKey)
-    .then(function(responses) {
-        return responses.json()
-        })
+    var coordApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + "219508d53c09462ba3344f4b9b6bbf02"
+    fetch(coordApi)
+    .then(function (response) {
+       response.json()
+   
     .then(function(data) {
         console.log(data)
+        
+        //create variables that will store Latitude and longitude jsonData results
+        var lat = data[0].lat
+        var lon = data[0].lon
+        //append to local storage
+        // localStorage.setItem("lat", lat);
+        // localStorage.setItem("lon", lon);
+    fetch( "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=782408e52c8c3294783b40fcfb93f113" + "&cnt=1")
+    .then(function(response) {
+         response.json()
+     .then(function(data) {
+         console.log(data)
+        currentCity.innerHTML = data.city.name
+        currentTemp.innerHTML = data.list[0].main.temp
         })
-    })
+     })
+     }) 
+})
 }
 search.addEventListener("click",function() {
     var citySearch = userInput.value;
     searchWeather(citySearch);
 })
-    searchWeather()
-    
-    // h2.innerText = jsonData[0].lat
-    // h3.innerText = jsonData[0].lon
     
