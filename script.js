7559f367034e45599144541994c3a1e9319c744b
omen <kybar089@gmail.com>
2022-10-12 22:50:16 -0500

    updated API display output

--- script.js
+++ script.js
@@ -1,38 +1,41 @@
 var userInput = document.getElementById('search-input')
 var search = document.getElementById('search-button')
-var currentCity = document.getElementById('current-city')
+var searchedCities = document.getElementById('searched-cities')
 var currentTemp = document.getElementById('current-temp')
+var currentWind = document.getElementById('current-wind')
+var currentHumidity = document.getElementById('current-humidity')
+var displayCity = document.querySelector('.card-title')
 
 function searchWeather(userInput) {
-    var apiKey = "219508d53c09462ba3344f4b9b6bbf02";
-    var coordApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + "219508d53c09462ba3344f4b9b6bbf02"
+        var apiKey = "219508d53c09462ba3344f4b9b6bbf02";
+        var coordApi = "http://api.openweathermap.org/geo/1.0/direct?q=" + userInput + "&appid=" + apiKey
     fetch(coordApi)
     .then(function (response) {
-       response.json()
+        response.json()
    
     .then(function(data) {
-        console.log(data)
-        
-        //create variables that will store Latitude and longitude jsonData results
+     //create variables that will store Latitude and longitude jsonData results
         var lat = data[0].lat
         var lon = data[0].lon
-        //append to local storage
-        // localStorage.setItem("lat", lat);
-        // localStorage.setItem("lon", lon);
     fetch( "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=782408e52c8c3294783b40fcfb93f113" + "&cnt=1")
     .then(function(response) {
-         response.json()
-     .then(function(data) {
-         console.log(data)
-        currentCity.innerHTML = data.city.name
-        currentTemp.innerHTML = data.list[0].main.temp
-        })
+        response.json()
+    .then(function(data) {
+        console.log(data)
+        localStorage.setItem("city-searched", data.city.name)
+        searchedCities.innerHTML = data.city.name
+        displayCity.innerHTML = data.city.name + "  (" + moment().format('L') + ")"
+        currentTemp.innerHTML = "Temp: " + data.list[0].main.temp + " °F"
+        currentWind.innerHTML = "Wind Speed: " + data.list[0].wind.speed + " MPH"
+        currentHumidity.innerHTML = "Humidity: " + data.list[0].main.humidity + "%"
+         })
      })
      }) 
 })
 }
-search.addEventListener("click",function() {
-    var citySearch = userInput.value;
-    searchWeather(citySearch);
+    search.addEventListener("click", function(event) {
+      event.preventDefault
+      var citySearch = userInput.value;
+      searchWeather(citySearch);
 })
     
